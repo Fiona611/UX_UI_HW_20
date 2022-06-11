89ab88f924f180498e7c2e27cf5d679609d07a98
Fiona611 <fionalin611@gmail.com>
2022-06-10 21:50:28 -0400

    code changes for week 20

---
+++ js/index.js
@@ -0,0 +1,25 @@
+$("#uxDesignButton").click(function() {
+    console.log("here");
+    $('html, body').animate({
+        scrollTop: $("#uXDesignGrid").offset().top
+    }, 2000);
+});
+
+$("#graphicDesignButton").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#uXDesignGrid").offset().top
+    }, 2000);
+});
+
+
+$("#AboutButton").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#uXDesignGrid").offset().top
+    }, 2000);
+});
+
+$("#contactButton").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#uXDesignGrid").offset().top
+    }, 2000);
+});
